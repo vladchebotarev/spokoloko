3098924e57dc83ba75dfdbd9e042cb9a551a6eeb
Vladislav <v.chebotarev@spokoloko.eu>
2018-04-09 09:01:59 +0200

    Separate search/venues.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -171,48 +171,9 @@
     <script src={{ asset('js/share/weekButtons.js') }}></script>
 @endif
 
-<script>
-
-    var startSlider = document.getElementById('price-range-slider');
-
-    if (startSlider) {
-        noUiSlider.create(startSlider, {
-            start: [{{$min_price}}, {{$max_price}}],
-            tooltips: true,
-            format: wNumb({
-                decimals: 0,
-                postfix: 'zł'
-            }),
-            connect: true,
-            range: {
-                'min': [0],
-                'max': [10000]
-            }
-        });
-    }
-
-    startSlider.noUiSlider.on('change.one', function(){
-        var prices = startSlider.noUiSlider.get();
-        $( "#minPrice" ).val(parseInt(prices[0].replace('zł','')));
-        $( "#maxPrice" ).val(parseInt(prices[1].replace('zł','')));
-        $('#search_form').submit();
-    });
-
-    $( "#select_city" ).change(function() {
-        $('#search_form').attr('action', $( "#select_city" ).val()).submit();
-    });
-
-    $( "#select_event_type" ).change(function() {
-        $('#search_form').submit();
-    });
-
-    $( "#clearFilters" ).on('click', function(){
-        $('input:checkbox').removeAttr('checked');
-    });
-
-
-
-</script>
+@if(Request::is('venues/*'))
+<script src={{ asset('js/search/venues.js') }}></script>
+@endif
 </body>
 
 </html>
