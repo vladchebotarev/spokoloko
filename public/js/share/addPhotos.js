1995cc5440993cfbbd3d8cc2a7e145ecbb715ae5
Mateusz Piątkowski <garadim12@gmail.com>
2018-04-10 11:34:03 +0200

    corrected addPhotos animations

--- public/js/share/addPhotos.js
+++ public/js/share/addPhotos.js
@@ -1,20 +1,38 @@
-let pinStyleOn = {
-	background: '#21BA45',
-	opacity: '1'
-};
+// let pinStyleOn = {
+// 	background: '#21BA45',
+// 	opacity: '1'
+// };
+//
+// let pinStyleOff = {
+// 	background: 'transparent',
+// 	opacity: '0'
+// };
 
-let pinStyleOff = {
-	background: 'transparent',
-	opacity: '0'
+function selected() {
+	if ($('.photo-upload-item').hasClass('selected')) {
+		return {
+			obj: $('.selected'),
+			exists: true
+		};
+	}
+	return {
+		obj: undefined,
+		exists: false
+	};
 };
 
 $('.photo-upload-item').click(function() {
-	let pins = $(this).find('.cover-photo');
+	// let pins = $(this).find('.cover-photo-sq');
+	let currentlySelected = selected();
 	if ($(this).hasClass('selected')) {
 		$(this).removeClass('selected');
-		pins.css(pinStyleOff);
+		// pins.css(pinStyleOff);
+	} else if (currentlySelected.exists) {
+		currentlySelected.obj.removeClass('selected');
+		$(this).addClass('selected');
+		// pins.css(pinStyleOn);
 	} else {
 		$(this).addClass('selected');
-		pins.css(pinStyleOn);
+		// pins.css(pinStyleOn);
 	}
 });
