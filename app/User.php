845ae11f8949ac605c0ba74d57889c5bb1b82a99
Vladislav <v.chebotarev@spokoloko.eu>
2018-05-09 15:00:54 +0200

    User last_name and confirmed fillable

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'email', 'password', 'provider', 'provider_id'
+        'first_name', 'last_name', 'email', 'password', 'provider', 'provider_id', 'confirmed'
     ];
 
     /**
