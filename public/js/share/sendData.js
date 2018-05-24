e9573d389570d821c17090ecb0d0bb15a8869ef5
Mateusz Piątkowski <garadim12@gmail.com>
2018-05-24 18:02:30 +0200

    uncomment

--- public/js/share/sendData.js
+++ public/js/share/sendData.js
@@ -2,7 +2,7 @@ function sendData() {
   $('#file-upload').attr('disabled', true);
   let data = new FormData($('.venue-form')[ 0 ]);
   console.log(photoArray[0].file);
-  // for(let i = 0; i < photoArray.length; ++i) data.append('images[]', photoArray[ i ].file);
+  for(let i = 0; i < photoArray.length; ++i) data.append('images[]', photoArray[ i ].file);
   $.ajax({
     url: '',
     data: data,
